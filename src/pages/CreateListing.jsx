f21f96a99290a31719699698910c9c0e3ba7ec86
Ali AlSaghir <alisalsaghir@gmail.com>
2023-09-24 12:05:52 +0300

    complete handleChange and handleSubmit functions including saving the form data to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
+import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 function CreateListing() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -13,28 +29,144 @@ function CreateListing() {
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
+    images: {},
   });
-
   const {
     type,
     name,
     bedrooms,
     bathrooms,
     parking,
-    furnished,
     address,
+    furnished,
     description,
     offer,
     regularPrice,
     discountedPrice,
-  } = formData.type;
+    images,
+  } = formData;
 
-  function handleChange() {}
+  console.log(formData);
+  console.log(type, offer);
+  function handleChange(e) {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    // Files
+    if (e.target.files) {
+      setFormData(prevState => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+    // Text/Boolean/Number
+    if (!e.target.files) {
+      setFormData(prevState => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    if (+discountedPrice >= +regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price should be less than regular price");
+      return;
+    }
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("maximum 6 images are allowed");
+      return;
+    }
+    let geolocation = {};
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&limit=3&q=${address}`
+    );
+    const data = await response.json();
+    if (data[0] === undefined) {
+      setLoading(false);
+      toast.error("please enter a correct address");
+      return;
+    }
+
+    geolocation.lat = data[0]?.lat ?? 0;
+    geolocation.lng = data[0]?.lon ?? 0;
+
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          snapshot => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          error => {
+            // Handle unsuccessful uploads
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map(image => storeImage(image))
+    ).catch(error => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+      userRef: auth.currentUser.uid,
+    };
+    delete formDataCopy.images;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    setLoading(false);
+    toast.success("Listing created");
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <main className="max-w-md px-2 mx-auto">
       <h1 className="text-3xl text-center mt-6 font-bold ">Create a Listing</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <p className="text-lg mt-6 font-semibold">Sell / Rent</p>
         <div className="flex">
           <button
@@ -61,7 +193,7 @@ function CreateListing() {
             }`}
             onClick={handleChange}
           >
-            Sell
+            Rent
           </button>
         </div>
         <p className="text-lg mt-6 font-semibold">Name</p>
@@ -198,9 +330,9 @@ function CreateListing() {
             No
           </button>
         </div>
-        <div className="flex items-center mb-6">
+        <div>
           <p className="text-lg font-semibold">Regular Price</p>
-          <div className="flex w-full justify-center space-x-6">
+          <div className="flex w-full justify-start items-center space-x-6 mb-6">
             <input
               type="number"
               id="regularPrice"
@@ -209,7 +341,7 @@ function CreateListing() {
               min={50}
               max={400000000}
               required
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+              className=" px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
             />
             {type === "rent" && (
               <div>
@@ -219,9 +351,9 @@ function CreateListing() {
           </div>
         </div>
         {offer && (
-          <div className="flex items-center mb-6">
+          <div>
             <p className="text-lg font-semibold">Discounted Price</p>
-            <div className="flex w-full justify-center space-x-6">
+            <div className="flex w-full justify-start items-center space-x-6 mb-6">
               <input
                 type="number"
                 id="discountedPrice"
@@ -230,7 +362,7 @@ function CreateListing() {
                 min={50}
                 max={400000000}
                 required={offer}
-                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                className="px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
               />
               {type === "rent" && (
                 <div>
@@ -240,9 +372,9 @@ function CreateListing() {
             </div>
           </div>
         )}
-        <div className="mb-6">
+        <div className="mb-8">
           <p className="text-lg font-semibold">Images</p>
-          <p className="text-gray-600">
+          <p className="text-gray-600 text-sm mb-1">
             The first image will be the cover (max 6 images)
           </p>
           <input
