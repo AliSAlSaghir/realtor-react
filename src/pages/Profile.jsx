8afb074c9275752e50df7f43755f49ff0bf62677
Ali AlSaghir <alisalsaghir@gmail.com>
2023-09-24 18:46:39 +0300

    fix my listings styling issue

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -141,7 +141,7 @@ function Profile() {
             <h2 className="text-2xl text-center font-semibold mb-6">
               My Listings
             </h2>
-            <ul className=" sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
+            <ul className=" sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2-xl:grid-cols-5 mt-6 mb-6 ">
               {listings.map(listing => (
                 <ListingItem
                   id={listing.id}
