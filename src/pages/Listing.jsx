0a0c171a2ef450443957c975386d90dc1da96aa1
Ali AlSaghir <alisalsaghir@gmail.com>
2023-09-25 11:56:02 +0300

    Add copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,11 +6,13 @@ import { toast } from "react-toastify";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 import Spinner from "../components/Spinner";
 
 function Listing() {
   const [isLoading, setIsLoading] = useState(false);
   const [listing, setListing] = useState(null);
+  const [isCopied, setIsCopied] = useState(false);
 
   const navigate = useNavigate();
   const params = useParams();
@@ -59,6 +61,24 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setIsCopied(true);
+          setTimeout(() => {
+            setIsCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {isCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          {" "}
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
